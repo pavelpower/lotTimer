3f21b8add09ccafe3dfe9f16b6b54fc610d93c3d
pavel.power <pavel.power@gmail.com>
2016-04-14 19:20:11 +0300

    added the function for Sync Server time

--- LotTimer.js
+++ LotTimer.js
@@ -2,14 +2,22 @@
  * Класс вычислений оставшегося времени для лотов
  * Время синхронезируется с сервером
  * @constructor
+ * @dependencies {jQuery}
+ * @param options {Object} - настройки
  */
-function LotTimer () {
+function LotTimer (options) {
 
-    this._serverTime;
+    this._syncUrl = options.syncUrl;
+    this._durationMode = options._durationMode;
 
 }
 
 LotTimer.prototype = {
+
+    /**
+     * URL для синхронизации серверного времени
+     */
+    _syncUrl: '',
 
     /**
      * Серверное время
@@ -26,9 +34,115 @@ LotTimer.prototype = {
      _remainderTime: null,
 
     /**
-     * Синхронезауия времени с сервером
+     * Если параметр выставлен в "ProlongationByLots" - проиходит обновление лотов
      */
-    syncServerTime: function () {},
+    _durationMode: null,
+
+    /**
+     * Возаращает URL со случайным параметром, чтобы не кешировать
+     * ответ от сервера, даже если он не поддерживает ETag
+     * @param url {String} - URL запроса
+     * @private
+     */
+    _getURLWithCMD: function (url) {
+        return url + '?' + Date.now();
+    },
+
+    /**
+     * Синхронезация времени с сервером
+     */
+    syncServerTime: function () {
+        var startSyncTime = this.getPresentTime();
+
+        $.get(this._getURLWithCMD(this._syncUrl)
+            .done(this._parseResponseServerTime.bind(this, startSyncTime)))
+            .fail(this.signalServerResponseFail.bind(this));
+    },
+
+    _parseResponseServerTime: function (response, startSyncTime) {
+        var timeSpentOnRequest;
+
+        if ($.type(response) !== 'object' || !response.endTimeSpan) {
+            this.signalServerResponseFail();
+            return;
+        }
+
+        // вычисляеться время потраченное на отправление и полуение запроса от сервера
+        // делиться на два - это интересно почему, видимо из практики вычитано
+        timeSpentOnRequest = Math.floor((this.getPresentTime() - startSyncTime) / 2);
+
+        // Вычисляеться еоличество прошедщих секунд по вычелсенному времени потраченному на запрос
+        // и добавляеться к серверному времени
+        this._remainderTime = addSeconds(response.endTimeSpan, Math.round(timeSpentOnRequest / 1000));
+
+        if (this._isTimeOver()) {
+            this.signalTimeIsOver();
+            return;
+        }
+
+        this.setServerTime(response.dateTime);
+
+    },
+
+    /**
+     * Сохранить серверное время
+     * @param dataTime {Date}
+     */
+    setServerTime: function (dataTime) {
+
+        if (dataTime) {
+            this._serverTime = new Date(
+                dataTime.Year,
+                dataTime.Month - 1,
+                dataTime.Day,
+                dataTime.Hour,
+                dataTime.Minute,
+                dataTime.Second,
+                dataTime.Millisecond + diffGetDate
+            );
+        }
+
+        this.signalServerTimeUpdated();
+
+        if (this._durationMode === 'ProlongationByLots') {
+            this.getLotsServerTime();
+        }
+    },
+
+    /**
+     * Проверка завершены ли торги или нет?
+     * @param remainingTime {timestamp}
+     * @private
+     * @return {Boolean}
+     */
+    _isTimeOver: function (remainingTime) {
+        var _remainingTime = remainingTime || this._remainderTime;
+
+        return _remainingTime.Hours == 0
+            && _remainingTime.Minutes == 0
+            && _remainingTime.Seconds == 0;
+    },
+
+    /**
+     * Сигнал о завершении торгов
+     * @override
+     */
+    signalTimeIsOver: function () {},
+
+    /**
+     * Запрос к серверу для получения серверного времени обвалился или пришел неверный ответ
+     * @override
+     */
+    signalServerResponseFail: function () {},
+
+    /**
+     * Сигнал о завершении оновления серверного времени
+     */
+    signalServerTimeUpdated: function () {
+        //this.showServerTime();
+        //this.showRemainingTime();
+    },
+
 
     /**
      * Получение данных лотов
